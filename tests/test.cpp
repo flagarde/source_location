6b00a94bdd0653fb9516f5e0315ff9ace83e7f79
flagarde <8627746+flagarde@users.noreply.github.com>
2021-05-23 05:03:00 +0800

    Clean

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,46 +1,30 @@
 ﻿#include <iostream>
 #include <source_location/source_location.hpp>
 
-
-
-
 source_location function()
 {
-  
-  
-  
-  
-  
   return source_location(source_location::current());
 }
 
 
-void toto(source_location loc)
+void function2(const source_location& loc)
 {
   std::cout << loc.file_name() << std::endl;
-    std::cout << loc.function_name() << std::endl;
-    std::cout << loc.line() << std::endl;
+  std::cout << loc.function_name() << std::endl;
+  std::cout << loc.line() << std::endl;
+  std::cout << loc.column() << std::endl;
 }
-
-
 
 int main()
 {
-    source_location loc = function();
-    std::cout << loc.file_name() << std::endl;
-    std::cout << loc.function_name() << std::endl;
-    std::cout << loc.line() << std::endl;
+  source_location loc = function();
+  std::cout << loc.file_name() << std::endl;
+  std::cout << loc.function_name() << std::endl;
+  std::cout << loc.line() << std::endl;
+  std::cout << loc.column() << std::endl;
+  
+  function2(loc);
     
-    
-    
-    
-    toto(loc);
-    
-    
-    
-    toto(source_location::current());
-    
-    
-    
-    return 0;
+  function2(source_location::current());
+  return 0;
 }
