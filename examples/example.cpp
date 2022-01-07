ae4e21e697c45abde255760fb701f98eb2f8e99d
flagarde <flagarde.programs@pm.me>
2022-01-08 07:25:34 +0800

    Add more examples

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <source_location/source_location.hpp>
 
+source_location src_clone(source_location a = source_location::current())
+{
+    return a;
+}
+
 int main()
 {
    source_location loc(source_location::current());
    std::cout << loc.file_name() << std::endl;
    std::cout << loc.function_name() << std::endl;
    std::cout << loc.line() << std::endl;
+   auto s2 = src_clone();          // location should point here 
+   std::cout
+        << s2.line() << ' ' << s2.function_name() << '\n';
 }
