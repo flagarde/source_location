c4e59028cf4b18a902fe773b4b91414d6a0bee29
flagarde <flagarde.programs@pm.me>
2022-01-11 11:32:39 +0800

    Better name for tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+
 #include <doctest/doctest.h>
 #include "source_location/source_location.hpp"
 
@@ -7,12 +9,14 @@ source_location src_clone(source_location a = source_location::current())
     return a;
 }
 
-TEST_CASE("Test source_location")
+std::string name="Test source_location with standard  : "+static_cast<std::string>(STANDARD);
+
+TEST_CASE(name.c_str())
 {
    source_location loc(source_location::current());
    std::cout<<"Line : "<< loc.line() <<" Column : "<< loc.column() << " File : "<<loc.file_name()<<" Function : "<<loc.function_name()<<std::endl;
-   CHECK(loc.line()==12);
+   CHECK(loc.line()==16);
    source_location s2 = src_clone();          // location should point here
-   CHECK(s2.line()==15);
+   CHECK(s2.line()==19);
    std::cout<<"Line : "<<  s2.line() <<" Column : "<<  s2.column() << " File : "<< s2.file_name()<<" Function : "<< s2.function_name()<<std::endl;
 }
