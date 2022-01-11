d7fdbd2664bde0568cdfbdb4a53f9133d33e2fa1
flagarde <flagarde.programs@pm.me>
2022-01-11 12:03:56 +0800

    Supress warnings

--- include/source_location/source_location.hpp
+++ include/source_location/source_location.hpp
@@ -12,10 +12,6 @@
   #include <experimental/source_location>
   using source_location = std::experimental::source_location;
 #else
-  #if __cplusplus >= 201103L
-    #include <cstdint>
-  #endif
-
   #if (__cplusplus >= 201103L && !defined(__clang__) && defined(__GNUC__) && __GNUC__>5) || (defined(_MSC_VER) && _MSC_VER >= 1900)
     #define NO_EXCEPT noexcept
     #define CONSTEXPR constexpr
@@ -28,17 +24,17 @@
   {
   public:
     #if (defined(__clang__) && __clang_major__ >= 9) || _MSC_VER > 1925
-      static source_location current(const long long& line = __builtin_LINE(),const long long& column = __builtin_COLUMN(), const char* file_name = __builtin_FILE(), const char* function_name = __builtin_FUNCTION())
+      static source_location current(const long int& line = __builtin_LINE(),const long int& column = __builtin_COLUMN(), const char* file_name = __builtin_FILE(), const char* function_name = __builtin_FUNCTION())
       {
         return source_location(line,column,file_name,function_name);
       }
     #elif defined(__GNUC__) &&  __GNUC__ >= 5
-      static source_location current(const long long& line = __builtin_LINE(),const long long& column = 0, const char* file_name = __builtin_FILE(), const char* function_name = __builtin_FUNCTION())
+      static source_location current(const long int& line = __builtin_LINE(),const long int& column = 0, const char* file_name = __builtin_FILE(), const char* function_name = __builtin_FUNCTION())
       {
         return source_location(line,column,file_name,function_name);
       }
     #else
-      static source_location current(const long long& line,const long long& column,const char* file_name, const char* function_name)
+      static source_location current(const long int& line = 0,const long int& column = 0 ,const char* file_name = "Not available !" ,const char* function_name = "Not available !")
       {
         return source_location(line,column,file_name,function_name);
       }
@@ -51,31 +47,23 @@
     #ifdef __cpp_rvalue_references
       source_location(source_location&& other) NO_EXCEPT : _line(other.line()), _column(other.column()), _file_name(other.file_name()), _function_name(other.function_name()){}
     #endif // __cpp_rvalue_references
-    #if __cplusplus >= 201103L
-      source_location(const std::uint_least32_t& line,const std::uint_least32_t& column, const char* file_name, const char* function_name): _line(line), _column(column), _file_name(file_name), _function_name(function_name){}
-    #else
-      source_location(const int& line,const int& column, const char* file_name, const char* function_name): _line(line), _column(column), _file_name(file_name), _function_name(function_name){}
-    #endif
+    source_location(const long int& line,const long int& column, const char* file_name, const char* function_name): _line(line), _column(column), _file_name(file_name), _function_name(function_name){}
 
     ~source_location() {}
-    #if __cplusplus >= 201103L
-      inline CONSTEXPR std::uint_least32_t line() const NO_EXCEPT { return _line; }
-      inline CONSTEXPR std::uint_least32_t column() const NO_EXCEPT { return _column; }
-    #else
-      inline CONSTEXPR int line() const NO_EXCEPT { return _line; }
-      inline CONSTEXPR int column() const NO_EXCEPT { return _column; }
+    #if defined(__GNUC__)
+      #pragma GCC diagnostic push
+      #pragma GCC diagnostic ignored "-Wpedantic"
     #endif
-    inline CONSTEXPR const char* file_name() const NO_EXCEPT { return _file_name; }
-    inline CONSTEXPR const char* function_name() const NO_EXCEPT { return _function_name; }
-
+      inline CONSTEXPR long int line() const NO_EXCEPT { return _line; }
+      inline CONSTEXPR long int column() const NO_EXCEPT { return _column; }
+      inline CONSTEXPR const char* file_name() const NO_EXCEPT { return _file_name; }
+      inline CONSTEXPR const char* function_name() const NO_EXCEPT { return _function_name; }
+    #if defined(__GNUC__)
+      #pragma GCC diagnostic pop
+    #endif
     private:
-      #if __cplusplus >= 201103L
-        const std::uint_least32_t _line{0};
-        const std::uint_least32_t _column{0};
-      #else
-        const int _line;
-        const int _column;
-      #endif
+      const int _line;
+      const int _column;
       const char* _file_name;
       const char* _function_name;
     };
