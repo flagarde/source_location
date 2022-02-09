cfa3ee05b21666138039f5283598f7e7395931ec
flagarde <flagarde.programs@pm.me>
2022-02-09 11:25:48 +0800

    Add tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -11,12 +11,21 @@ source_location src_clone(source_location a = source_location::current())
 
 std::string name="Test source_location with standard  : "+static_cast<std::string>(STANDARD);
 
+source_location function()
+{
+  source_location loc(source_location::current());
+  return loc;
+}
+
 TEST_CASE(name.c_str())
 {
    source_location loc(source_location::current());
    std::cout<<"Line : "<< loc.line() <<" Column : "<< loc.column() << " File : "<<loc.file_name()<<" Function : "<<loc.function_name()<<std::endl;
-   CHECK(loc.line()==16);
+   CHECK(loc.line()==22);
    source_location s2 = src_clone();          // location should point here
-   CHECK(s2.line()==19);
+   CHECK(s2.line()==25);
    std::cout<<"Line : "<<  s2.line() <<" Column : "<<  s2.column() << " File : "<< s2.file_name()<<" Function : "<< s2.function_name()<<std::endl;
+   source_location from_function = function();
+   CHECK(from_function.line()==16);
+   std::cout<<"Line : "<<  from_function.line() <<" Column : "<<  from_function.column() << " File : "<< from_function.file_name()<<" Function : "<< from_function.function_name()<<std::endl;
 }
